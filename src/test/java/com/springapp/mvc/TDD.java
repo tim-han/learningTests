8aac07c93715e5c16186ff8ae15306b618e45258
tim-han <juntz@daum.net>
2015-01-28 18:18:30 +0900

    edit .gitignore update TDD

---
+++ src/test/java/com/springapp/mvc/TDD.java
@@ -0,0 +1,39 @@
+package com.springapp.mvc;
+
+import com.springapp.mvc.tdd.Money;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yjhan on 15. 1. 26..
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring/applicationContext.xml")
+public class TDD {
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+}
